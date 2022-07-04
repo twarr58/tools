dcb9285becad7881fac7ae2d457a828f4554d33a
Jan Dvořák <twarr58@lol.localdomain>
2022-07-05 01:35:36 +0200

    chg

--- clean.py
+++ clean.py
@@ -20,13 +20,13 @@ def list_dir(path, ext='*.*', recursive=False) -> list:
         work_list.append(file)
     return work_list
 
-def get_file_stat(file, out='kb'):
+def get_file_size(file, out='kb'):
     if out == 'kb': out_div = 1024; ext = 'KB'
     elif out == 'mb': out_div = 1024*1024; ext = 'MB'
-    return f'{file} : {round(os.stat(file).st_size/out_div,2)}KB'
+    return f'{file} : {round(os.stat(file).st_size/out_div,2)}{ext}}'
         
 # print(get_file_stat('/home/twarr58/.bash_history'))
 for file in list_dir('/usr/bin', recursive=True):
-    print(get_file_stat(file))
+    print(get_file_size(file))
 
 
