8449057c386092ad5b79c85911432cf3009b0c9f
twarr58 <twarr58@gmail.com>
2022-07-05 01:37:10 +0200

    Update clean.py

--- clean.py
+++ clean.py
@@ -23,7 +23,7 @@ def list_dir(path, ext='*.*', recursive=False) -> list:
 def get_file_size(file, out='kb'):
     if out == 'kb': out_div = 1024; ext = 'KB'
     elif out == 'mb': out_div = 1024*1024; ext = 'MB'
-    return f'{file} : {round(os.stat(file).st_size/out_div,2)}{ext}}'
+    return f'{file} : {round(os.stat(file).st_size/out_div,2)}{ext}'
         
 # print(get_file_stat('/home/twarr58/.bash_history'))
 for file in list_dir('/usr/bin', recursive=True):
